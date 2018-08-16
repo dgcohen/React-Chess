c93b8223a4e64030f4d5771d70a65bb6ee5d234b
Diego Cohen <diegocohen@Diegos-MBP.home>
2018-08-15 21:55:08 -0400

    Restore index css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/styles.css';
-import './assets/css/vendor.css';
+import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
