519fc69bbd5a678f4569a5ff42d4323a38667e1b
Diego Cohen <dcohen@digitalpulp.com>
2018-08-16 11:35:56 -0400

    Capitalize board in import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Board from './components/board';
+import Board from './components/Board';
 import initializeBoard from './helpers/initializeBoard.js';
 
 // components
